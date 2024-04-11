e7a716fee74739b215c3c425f027b1b0e3deeb34
Robertobarbas23 <tandemrobertoaranjuez@gmail.com>
2024-04-11 13:35:07 +0200

    empiezo logica

---
+++ main2.js
@@ -0,0 +1,23 @@
+//recoger la informacion
+ function getInfo(){
+    //devuelve el texto
+    console.log('texto ingresado')
+    return'texto ingresado'
+ }
+//almacenarla en localStorage
+function setLocalStorage(){
+    //almacenarla en LocalStorage
+    return true;
+}
+//leerla y sacarla por pantalla
+function showInfo(){
+    //leerla y mostrtrarla por pantalla
+    return true;
+}
+//Limpiarla
+function cleanInfo(){
+    
+}
+    //limpiar el Local Storage y limpiar la pantalla 
+
+
